cc64796fac25fb0260fe5e4d6627af9ce9d2eb58
= <mertbuldur@Mert-MacBook-Pro.local>
2019-03-14 17:12:53 +0300

    Delete Index.php

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
 <?php
-include("src/translateInterface.php");
-include("src/translate.php");
-use TranslateAPI\translate;
-$translate = new translate("trnsl.1.1.20181116T200129Z.775558096056e445.a0cb98993d244e1a93e7aa435b7a0f60a6b5ab4d","tr-en");
-echo $translate->translate("merhaba dünya");
