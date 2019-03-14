b725d566e93ea9ca27cba8a12fbc7fbe76ee05a8
= <mertbuldur@Mert-MacBook-Pro.local>
2019-03-14 17:12:13 +0300

    PHPDocs Added

--- src/translate.php
+++ src/translate.php
@@ -1,13 +1,30 @@
 <?php
 namespace TranslateAPI;
+/**
+ * Class translate
+ * @package TranslateAPI
+ */
 class translate implements translateInterface
 {
+    /**
+     * @var string
+     */
     public $url = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+
+    /**
+     * translate constructor.
+     * @param $apiKey
+     * @param $targetLanguage
+     */
     public function __construct($apiKey, $targetLanguage)
     {
         $this->url = $this->url."?lang=".$targetLanguage."&key=".$apiKey;
     }
 
+    /**
+     * @param $text
+     * @return string
+     */
     public function translate($text)
     {
         $text = "text=".$text;
@@ -17,6 +34,11 @@ class translate implements translateInterface
     }
 
 
+    /**
+     * @param $address
+     * @param $data
+     * @return bool|string
+     */
     public function HTTPPoster($address, $data)
     {
         $ch = curl_init();
